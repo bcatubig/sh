b349e8c7e6aa04fae7fc82ebcdf1a8056c155096
Brandon Catubig <bcatubig@clubhouse.io>
2020-08-24 13:22:39 -0500

    [sh] Add stdin support and context support

--- sh.go
+++ sh.go
@@ -2,27 +2,35 @@ package sh
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"os/exec"
 )
 
-// Args set the arguments passed to the binary
+// Args sets the arguments passed to the binary
 func Args(args ...string) func(*Command) {
 	return func(c *Command) {
 		c.args = args
 	}
 }
 
-// Environment set the command subshell environment
+// Environment sets the command subshell environment
 func Environment(env map[string]string) func(*Command) {
 	return func(c *Command) {
 		c.environment = env
 	}
 }
 
-// WorkingDir set the command subshell current working directory
+// Input sets stdin input to the process
+func Input(input io.Reader) func(*Command) {
+	return func(c *Command) {
+		c.input = input
+	}
+}
+
+// WorkingDir sets the command subshell current working directory
 func WorkingDir(dir string) func(*Command) {
 	return func(c *Command) {
 		c.workingDir = dir
@@ -51,6 +59,7 @@ type Command struct {
 	args               []string
 	environment        map[string]string
 	expectedReturnCode int
+	input              io.Reader
 	workingDir         string
 	writers            []io.Writer
 }
@@ -62,6 +71,7 @@ func NewCommand(binary string, opts ...func(command *Command)) *Command {
 		args:               []string{},
 		environment:        nil,
 		expectedReturnCode: 0,
+		input:              nil,
 		workingDir:         "",
 		writers:            []io.Writer{},
 	}
@@ -76,7 +86,15 @@ func NewCommand(binary string, opts ...func(command *Command)) *Command {
 
 // Run will execute a shell command and wait for it to finish, returning stdout/stderr combined output
 func (c *Command) Run() (*RunOutput, error) {
+	return runCommand(nil, c)
+}
 
+// Run will execute a shell command with a context and wait for it to finish, returning stdout/stderr combined output
+func (c *Command) RunWithContext(ctx context.Context) (*RunOutput, error) {
+	return runCommand(ctx, c)
+}
+
+func runCommand(ctx context.Context, c *Command) (*RunOutput, error) {
 	// Create a buffer to write stdout/stderr to
 	buf := &bytes.Buffer{}
 
@@ -84,11 +102,19 @@ func (c *Command) Run() (*RunOutput, error) {
 	mw := io.MultiWriter(append(c.writers, buf)...)
 
 	// Create a low level Command object
-	cmd := exec.Command(c.binary, c.args...)
+	var cmd *exec.Cmd
+	if ctx == nil {
+		cmd = exec.Command(c.binary, c.args...)
+	} else {
+		cmd = exec.CommandContext(ctx, c.binary, c.args...)
+	}
 
 	// Configure the command to write to our multi-writer
 	cmd.Stdout = mw
 	cmd.Stderr = mw
+
+	// Configure stdin
+	cmd.Stdin = c.input
 
 	// Configure the working directory
 	cmd.Dir = c.workingDir
@@ -103,33 +129,32 @@ func (c *Command) Run() (*RunOutput, error) {
 
 	if err != nil {
 		// Validate that non-zero is equal to expectedExitCode if set
-		var e *exec.ExitError
-		if errors.As(err, &e) {
-			rc := e.ExitCode()
-			if rc == c.expectedReturnCode {
-				return &RunOutput{
-					ReturnCode: rc,
-					Output:     buf,
-				}, nil
-			} else {
-				return &RunOutput{
-					ReturnCode: rc,
-					Output:     buf,
-				}, err
-			}
+		rc, rcErr := getReturnCode(err)
+
+		if rcErr != nil {
+			return &RunOutput{
+				ReturnCode: 1,
+				Output:     buf,
+			}, err
+		}
+
+		if rc != c.expectedReturnCode {
+			return &RunOutput{
+				ReturnCode: rc,
+				Output:     buf,
+			}, fmt.Errorf("unexpected exit code: expected: %d, got %d", c.expectedReturnCode, rc)
 		}
 
 		return &RunOutput{
-			ReturnCode: 1,
+			ReturnCode: rc,
 			Output:     buf,
-		}, err
+		}, nil
 	}
 
 	return &RunOutput{
 		ReturnCode: 0,
 		Output:     buf,
 	}, nil
-
 }
 
 // RunOutput returns the process return code and combined stdout/stderr output
@@ -144,4 +169,13 @@ func generateEnvSlice(env map[string]string) []string {
 		result = append(result, fmt.Sprintf("%s=%s", k, v))
 	}
 	return result
+}
+
+func getReturnCode(err error) (int, error) {
+	var e *exec.ExitError
+	if errors.As(err, &e) {
+		return e.ExitCode(), nil
+	}
+
+	return 0, fmt.Errorf("not a valid ExitError")
 }
